f5b04ea1a8b7dc10cac957be233a53a0836054dc
caretak3r <caretak3r@users.noreply.github.com>
2026-09-22 09:51:25 -0400

    Add 2026-09-22 latest research digest

--- content/latest/_meta.js
+++ content/latest/_meta.js
@@ -1,5 +1,6 @@
 export default {
   index: 'About digests',
+  '2026-09-22': '2026-09-22 digest',
   '2026-09-21': '2026-09-21 digest',
   '2026-09-18': '2026-09-18 digest',
   '2026-09-17': '2026-09-17 digest',
